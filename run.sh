827a2b80f1fbc0315b2161aa094698ca59e682ac
陈斌 <353549444@qq.com>
2018-04-18 16:37:39 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # 停止admin容器
-docker stop admin
+(docker stop admin && echo "stop success") || echo "stop error";
 
 # 删除damin容器
-docker rm admin
+(docker rm admin && echo "rm success") || echo "rm error";
 
 # 删除原来镜像
-docker rmi hongfund/admin
+(docker rmi admin && echo "rmi success") || echo "rmi error";
 
 # 生成新的镜像
-./mvnw install dockerfile:build
+(docker build -t admin . && echo "rmi success") || echo "rmi error";
 
 # 运行admin容器
-docker run --name admin -p 8899:8899 -d hongfund/admin
+docker run --name admin \
+    -p 8899:8899 \
+    -d \
+    -e TZ=Asia/Shanghai \
+    admin
